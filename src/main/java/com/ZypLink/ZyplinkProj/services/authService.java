f98f548d25a4c52f77f98276ab057d9ec55d34c9
Samarth-1211 <samarthsharma1211@gmail.com>
2026-02-03 22:33:36 +0530

    Unusual comment reduction

--- src/main/java/com/ZypLink/ZyplinkProj/services/authService.java
+++ src/main/java/com/ZypLink/ZyplinkProj/services/authService.java
@@ -1,24 +1,26 @@
 package com.ZypLink.ZyplinkProj.services;
 
 import java.time.LocalDateTime;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Service;
 
+import org.springframework.stereotype.Service;
 import com.ZypLink.ZyplinkProj.config.SecurityConfig;
 import com.ZypLink.ZyplinkProj.dto.AuthResponseDTO;
+
 import com.ZypLink.ZyplinkProj.dto.LoginRequestDTO;
 import com.ZypLink.ZyplinkProj.dto.OtpVerificationRequest;
 import com.ZypLink.ZyplinkProj.dto.UserDTO;
+
 import com.ZypLink.ZyplinkProj.entities.User;
 import com.ZypLink.ZyplinkProj.repositories.UserRepository;
 import com.ZypLink.ZyplinkProj.utils.JwtUtils;
-import com.ZypLink.ZyplinkProj.utils.OtpUtil;
 
+import com.ZypLink.ZyplinkProj.utils.OtpUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
